af754aa1d6fbfc5aee739e406a8eaf926b5487fa
SusanaJanine <susanajaninevelezflores@gmail.com>
2026-01-29 18:45:47 +0000

    confirm

--- main4.py
+++ main4.py
@@ -1,8 +1,8 @@
-Numero = int(input("Ingrese un numero: "))
+Numero = int(input("Ingrese un numero: ")) # Se solicita al usuario que ingrese un numero
 
-if Numero > 0:
+if Numero > 0: # Se determina si el numero ingresado es mayor a cero entonces se imprime que es un numero positivo
     print("El numero es positivo")
-elif Numero < 0:
+elif Numero < 0: # Pero si el numero que se ingreso es menor a cero entonces se imprime que es un numero negativo
     print("El numero es negativo")
-else:
+else: # Si no se cumple ninguna de las dos condiciones anteriores entonces el numero debera ser cero
     print("El numero es cero")
--- main5.py
+++ main5.py
@@ -1,5 +1,5 @@
-Tabla = int(input("Ingrese un numero para multiplicar: "))
+Tabla = int(input("Ingrese un numero para multiplicar: ")) # Se solicita al usuario que ingrese un numero para generar su tabla de multiplicar
 
-for i in range(1, 11):
-    resultado = Tabla * i
-    print(f"{Tabla} x {i} = {resultado}")
+for i in range(1, 11): # El bucle for solo puede recorrer valores del 1 al 10 para generar la tabla de multiplicar
+    resultado = Tabla * i # Se realiza la operacion de multiplicacion entre el numero ingresado y el valor actual de i
+    print(f"{Tabla} x {i} = {resultado}") # Se imprime la multiplicacion de forma legible y el resultado obtenido
