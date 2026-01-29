f7e28cc4cdf1a0759cdfba6364db81087bf776dd
SusanaJanine <susanajaninevelezflores@gmail.com>
2026-01-29 18:17:01 +0000

    confirmacion

---
+++ main4.py
@@ -0,0 +1,8 @@
+Numero = int(input("Ingrese un numero: "))
+
+if Numero > 0:
+    print("El numero es positivo")
+elif Numero < 0:
+    print("El numero es negativo")
+else:
+    print("El numero es cero")
---
+++ main5.py
@@ -0,0 +1,5 @@
+Tabla = int(input("Ingrese un numero para multiplicar: "))
+
+for i in range(1, 11):
+    resultado = Tabla * i
+    print(f"{Tabla} x {i} = {resultado}")
