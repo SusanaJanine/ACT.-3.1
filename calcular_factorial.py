6e0a01538199cdb9b6661b647f17c0b70cf603b5
SusanaJanine <susanajaninevelezflores@gmail.com>
2026-02-03 17:54:25 +0000

    confirm

--- calcular_factorial.py
+++ calcular_factorial.py
@@ -1,7 +1,7 @@
 numero = int(input("Ingrese un número para calcular su factorial: "))
 def calcular_factorial(n):
     if n < 0:
-        return "El factorial no está definido para números negativos."
+        return "El factorial no funciona con números negativos."
     elif n == 0 or n == 1:
         return 1
     else:
