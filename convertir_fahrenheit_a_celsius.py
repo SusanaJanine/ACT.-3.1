3cb3249e755432a05bf5511a9b93612caa90a0ff
SusanaJanine <susanajaninevelezflores@gmail.com>
2026-02-09 16:26:51 +0000

    confirmar

--- convertir_fahrenheit_a_celsius.py
+++ convertir_fahrenheit_a_celsius.py
@@ -1,7 +1,7 @@
-def fahrenheit_a_celsius(fahrenheit):
-    celsius = (fahrenheit - 32) * 5 / 9
-    return celsius
+def fahrenheit_a_celsius(fahrenheit): # Se define la función que convierte Fahrenheit a Celsius
+    celsius = (fahrenheit - 32) * 5 / 9 # Se realiza la conversión utilizando la fórmula (F - 32) * 5/9
+    return celsius # Se devuelve el resultado de la conversión
 
-fahrenheit = float(input("Ingrese la temperatura en grados Fahrenheit: "))
-resultado = fahrenheit_a_celsius(fahrenheit)
-print(f"La temperatura en grados Celsius es: {resultado:.2f} °C")
+fahrenheit = float(input("Ingrese el numero de grados Fahrenheit: ")) # Se solicita al usuario que ingrese la temperatura en grados Fahrenheit
+resultado = fahrenheit_a_celsius(fahrenheit) # Se llama a la función para convertir la temperatura
+print(f"La temperatura en grados Celsius es: {resultado:.2f} °C") # Se imprime el resultado
--- factorial2.py
+++ factorial2.py
@@ -1,11 +1,11 @@
-def factorial(n):
-    if n < 0:
-        return 1
+def factorial(n): # Se define la función que calcula el factorial de un número
+    if n < 0: # Se verifica si el número es negativo, ya que el factorial no funciona para números negativos
+        return 1 # Se devuelve 1 para el caso de números negativos, aunque en realidad el factorial no está definido para ellos
     else:
-        return n * factorial(n - 1)
+        return n * factorial(n - 1) # Se realiza la recursión multiplicando el número por el factorial del número anterior hasta llegar a 0.
     
-numero = int(input("Escriba un numero para calcular su factorial: "))
-if numero < 0:
-    print("El factorial no se puede calcular en números negativos.")
+numero = int(input("Escriba un numero para calcular su factorial: ")) # Se solicita al usuario que ingrese un número para calcular su factorial
+if numero < 0: # Se verifica si el número es negativo o positivo, ya que el factorial no se puede calcular para números negativos
+    print("El factorial no se puede calcular en números negativos.") # Se muestra un mensaje de error indicando que el factorial no se puede calcular para números negativos.
 else:
-    print(f"El factorial de {numero} es: {factorial(numero)}")
+    print(f"El factorial de {numero} es: {factorial(numero)}") # Se imprime el resultado del factorial utilizando una f-string para mostrar el número ingresado y su factorial.
